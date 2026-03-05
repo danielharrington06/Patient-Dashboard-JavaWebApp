e74a2e00c4286b1ecfaefffce66db34aaffd3086
Daniel Harrington <danielharrington06@outlook.com>
2026-03-05 18:07:53 +0000

    made field headings in patientRecord more clear

--- src/main/java/uk/ac/ucl/model/DataFrame.java
+++ src/main/java/uk/ac/ucl/model/DataFrame.java
@@ -29,18 +29,44 @@ class DataFrame {
         return columns.get(0).getSize();
     }
 
-    public String getValue(String field, int row) {
-        int i = getColumnNames().indexOf(field);
+    public String getValue(String columnName, int row) {
+        int i = getColumnNames().indexOf(columnName);
         return columns.get(i).getRowValue(row);
     }
 
-    public void putValue(String field, int row, String value) {
-        int i = getColumnNames().indexOf(field);
+    public void putValue(String columnName, int row, String value) {
+        int i = getColumnNames().indexOf(columnName);
         columns.get(i).setRowValue(row, value);
     }
 
-    public void addValue(String field, String value) {
-        int i = getColumnNames().indexOf(field);
+    public void addValue(String columnName, String value) {
+        int i = getColumnNames().indexOf(columnName);
         columns.get(i).addRowValue(value);
+    }
+
+    public String formatColumnName(String columnName) {
+        switch (columnName.toUpperCase()) {
+            case "ID" -> {return "Patient ID";}
+            case "BIRTHDATE" -> {return "Date of Birth";}
+            case "DEATHDATE" -> {return "Date of Death";}
+            case "SSN" -> {return "SSN";}
+            case "DRIVERS" -> {return "Driver's Licence Number";}
+            case "PASSPORT" -> {return "Passport Number";}
+            case "PREFIX" -> {return "Prefix";}
+            case "FIRST" -> {return "Firstname";}
+            case "LAST" -> {return "Lastname";}
+            case "SUFFIX" -> {return "Suffix";}
+            case "MAIDEN" -> {return "Maiden Name";}
+            case "MARITAL" -> {return "Marital Status";}
+            case "RACE" -> {return "Race";}
+            case "ETHNICITY" -> {return "Ethnicity";}
+            case "GENDER" -> {return "Gender";}
+            case "BIRTHPLACE" -> {return "Birthplace";}
+            case "ADDRESS" -> {return "Street Address";}
+            case "CITY" -> {return "City";}
+            case "STATE" -> {return "State";}
+            case "ZIP" -> {return "Zip Code";}
+            default -> throw new IllegalArgumentException("Unknown field: " + columnName);
+        }
     }
 }
